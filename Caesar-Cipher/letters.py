7ee1ea0d9a157fbac241c5a9e40c2c189da42231
rambabu-1929 <rambabu2k17.21@gmail.com>
2024-05-14 23:46:16 +0530

    caesar initial start

---
+++ Caesar-Cipher/letters.py
@@ -0,0 +1,7 @@
+def alphas():
+    alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return alpha
+              
+def decodes():
+    decoder = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return decoder
